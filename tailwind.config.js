bfad37a0cd18e9d970fa7d65e18639ca47120e96
ri-commits <kartashourychard@gmail.com>
2023-05-13 16:05:21 +0300

    Created Hero and Demo layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
       fontFamily: {
-        satoshi: ["satoshi", "sans-serif"],
+        satoshi: ["Satoshi", "sans-serif"],
         inter: ["Inter", "sans-serif"],
       },
     },
