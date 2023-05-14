c3fd5a08f62cb2f275474ace2447f7d14a57ae26
ri-commits <kartashourychard@gmail.com>
2023-05-14 16:50:33 +0300

    Input implemented

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,13 +3,21 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    alert("Submited");
+  };
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search */}
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
@@ -19,13 +27,23 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Enter a URL"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer"
           />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ⎆
+          </button>
         </form>
+
+        {/* Brows URL History */}
       </div>
+
+      {/* Display Results */}
     </section>
   );
 };
